5f0fa5a5265ed3447159e8f892cb74e4a38b566b
Kyle Nazario <kylebnazario@gmail.com>
2025-09-11 16:07:18 -0600

    Initial Commit

---
+++ Film Cache/Film_CacheApp.swift
@@ -0,0 +1,17 @@
+//
+//  Film_CacheApp.swift
+//  Film Cache
+//
+//  Created by Kyle Nazario on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Film_CacheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
