6b1184b349bb783f8b683570b83d0fc31febb988
Kyle Nazario <kylebnazario@gmail.com>
2025-09-18 17:34:06 -0600

    Displays details view side-by-side

--- Film Cache/ContentView.swift
+++ Film Cache/ContentView.swift
@@ -10,13 +10,14 @@ import SwiftUI
 struct ContentView: View {
     
     @StateObject() private var controller = ContentViewController()
+    @State private var selectedMovieID: FCMovie.ID?
     
     var body: some View {
         Group {
             if controller.loading {
                 ProgressView()
             } else {
-                FCListDetailsSplitPane(movies: controller.movies)
+                FCListDetailsSplitPane(movies: controller.movies, selectedMovieID: $selectedMovieID)
             }
         }
         .navigationTitle(APP_NAME)
@@ -25,18 +26,27 @@ struct ContentView: View {
 
 struct FCListDetailsSplitPane: View {
     let movies: [FCMovie]
-    
-    @State private var selectedMovieID: FCMovie.ID?
+    @Binding var selectedMovieID: FCMovie.ID?
     
     var body: some View {
         HStack {
             FCMovieList(movies: movies, selectedMovieID: $selectedMovieID)
+            if let selectedMovie {
+                FCMovieDetails(movie: selectedMovie)
+            }
         }
+    }
+    
+    private var selectedMovie: FCMovie? {
+        movies.first { $0.id == selectedMovieID }
     }
 }
 
 #Preview {
-    FCListDetailsSplitPane(movies: [mockMovieCaughtStealing.toFCMovie()])
+    FCListDetailsSplitPane(
+        movies: [mockMovieCaughtStealing.toFCMovie()],
+        selectedMovieID: .constant(mockMovieCaughtStealing.id)
+    )
         .frame(width: 700, height: 500)
         .navigationTitle(APP_NAME)
 }
