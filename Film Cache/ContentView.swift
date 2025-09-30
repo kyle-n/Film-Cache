23326caec6d49b2a98391249d2b1cb18270e94d5
Kyle Nazario <kylebnazario@gmail.com>
2025-09-30 10:55:04 -0600

    Shows/hides details pane depending on store

--- Film Cache/ContentView.swift
+++ Film Cache/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ReSwift
 
 struct ContentView: View {
     @StateObject() private var controller = ContentViewController()
@@ -15,7 +16,7 @@ struct ContentView: View {
             if controller.loading {
                 ProgressView()
             } else {
-                FCListDetailsSplitPane(movies: controller.movies)
+                FCListDetailsSplitPane(movies: controller.movies, showDetails: controller.showDetails)
             }
         }
         .navigationTitle(APP_NAME)
@@ -25,15 +26,43 @@ struct ContentView: View {
     }
 }
 
+final class ContentViewController: ObservableObject, StoreSubscriber {
+    @Published private(set) var movies: [FCMovie] = []
+    @Published private(set) var loading = false
+    @Published private(set) var showDetails = false
+
+    init() {
+        DispatchQueue.main.async {
+            fcStore.subscribe(self)
+            fcStore.dispatch(FCAction.appOpened())
+        }
+    }
+
+    deinit {
+        fcStore.unsubscribe(self)
+    }
+
+    func newState(state: FCAppState) {
+        DispatchQueue.main.async {
+            self.movies = state.movies
+            self.loading = state.loadingMovies
+            self.showDetails = state.selectedMovieID != nil
+        }
+    }
+}
+
 struct FCListDetailsSplitPane: View {
     let movies: [FCMovie]
+    let showDetails: Bool
 
     var body: some View {
         GeometryReader { geo in
             HSplitView {
                 FCMovieList(movies: movies)
-                FCMovieDetailsContainer()
-                    .frame(width: max(geo.size.width / 3, 300))
+                if showDetails {
+                    FCMovieDetailsContainer()
+                        .frame(width: max(geo.size.width / 3, 300))
+                }
             }
         }
     }
@@ -42,13 +71,15 @@ struct FCListDetailsSplitPane: View {
 #Preview {
     VStack {
         FCListDetailsSplitPane(
-            movies: [mockMovieCaughtStealing.toFCMovie()]
+            movies: [mockMovieCaughtStealing.toFCMovie()],
+            showDetails: false
         )
         .frame(width: 700, height: 350)
         .navigationTitle(APP_NAME)
         .border(Color.white)
         FCListDetailsSplitPane(
-            movies: [mockMovieCaughtStealing.toFCMovie()]
+            movies: [mockMovieCaughtStealing.toFCMovie()],
+            showDetails: true
         )
         .frame(width: 700, height: 350)
         .navigationTitle(APP_NAME)
--- Film Cache/ContentViewController.swift
+++ Film Cache/ContentViewController.swift
@@ -5,31 +5,4 @@
 //  Created by Kyle Nazario on 9/11/25.
 //
 
-import Combine
-import ReSwift
-import SwiftUI
 
-final class ContentViewController: ObservableObject, StoreSubscriber {
-    @Published private(set) var movies: [FCMovie] = []
-    @Published private(set) var loading = false
-
-    private var cancellables = Set<AnyCancellable>()
-
-    init() {
-        DispatchQueue.main.async {
-            fcStore.subscribe(self)
-            fcStore.dispatch(FCAction.appOpened())
-        }
-    }
-
-    deinit {
-        fcStore.unsubscribe(self)
-    }
-
-    func newState(state: FCAppState) {
-        DispatchQueue.main.async {
-            self.movies = state.movies
-            self.loading = state.loadingMovies
-        }
-    }
-}
